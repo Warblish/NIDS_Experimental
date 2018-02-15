f85c7d000053c0be9969b0123bb208d94cdfc1e0
Huimin-Hu <33388508+Huimin-Hu@users.noreply.github.com>
2018-02-15 11:47:49 -0500

    Add files via upload

--- fcns/NIDS.m
+++ fcns/NIDS.m
@@ -1,21 +1,21 @@
 classdef NIDS
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%    Decentralized Optimization with Network InDependent Stepsizes                             
-%      minimize S(x) + R(x)   subject to Wx = x                                              
-%    S is differentiable and R is proximable, W is the given mixing matrix             
-%
-%
-%    Three methods are implemented: PG-EXTRA, NIDS, and DIGing         
-%    Reference: 
-%       
-%
-%    Contact:
-%       Zhi Li lizhi@msu.edu
-%       Wei Shi ??? 
-%       Ming Yan myan@math.msu.edu
-% 
-%       Downloadable from ?????                                                              %%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %    Decentralized Optimization with Network InDependent Stepsizes
+    %      minimize S(x) + R(x)   subject to Wx = x
+    %    S is differentiable and R is proximable, W is the given mixing matrix
+    %
+    %
+    %    Three methods are implemented: PG-EXTRA, NIDS, and DIGing
+    %    Reference:
+    %
+    %
+    %    Contact:
+    %       Zhi Li lizhi@msu.edu
+    %       Wei Shi ???
+    %       Ming Yan myan@math.msu.edu
+    %
+    %       Downloadable from ?????                                                              %%
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     properties
         getGradS   =  @(x) 0;            % gradient of S      gradS: x  --> grad(S)(x)
         getProxR   =  @(x,t) x;          % prox of R          proxR: x,t  --> prox(t.R)(x)
@@ -25,7 +25,7 @@ classdef NIDS
     
     methods
         function e = E(this, x)    % return the objective function value
-            e   = this.getS(x) + this.getR(x);                       
+            e   = this.getS(x) + this.getR(x);
         end
         
         function [out] = minimize(this, paras)
@@ -37,102 +37,109 @@ classdef NIDS
             iter    = paras.iter;   % the maximum number of iterations
             x0      = paras.x0;     % the initial x
             alpha   = paras.alpha;  % the stepsize, which can be different for different nodes
-            method  = paras.method; % the method that used 
+            method  = paras.method; % the method that used
             W       = paras.W;      % the mixing matrix
+            [~,~,len_W] = size(W);
             
             Q       = diag(1./alpha);
             eye_L   = eye(n);
-            I_W     = eye_L - W;
+            for i = 1:len_W
+                I_W(:,:,i) = eye_L - W(:,:,i);
+            end
             
             if isfield(paras, 'forcetTildeW') && paras.forcetTildeW
-                tildeW = (eye_L + W)/2;
+                for i = 1:len_W
+                    tildeW(:,:,i) = (eye_L + W(:,:,i))/2;
+                end
             else
                 switch method
                     case {'NIDS', 'NIDS-adaptive', 'NIDSS', 'NIDSS-adaptive'}  % NIDS
                         c = paras.c;      % the parameter c in  NIDS
-
-                        tildeW = eye_L - c * diag(alpha) * I_W;
+                        
+                        for i = 1:len_W
+                            tildeW(:,:,i) = eye_L - c(i) * diag(alpha) * I_W(:,:,i);
+                        end
                     case {'PG-EXTRA', 'PGEXTRA', 'EXTRA'}
                         tildeW = (eye_L + W)/2;
                 end
             end
             
-            x_cur   = x0;           % the current iterate   x^k 
+            x_cur   = x0;           % the current iterate   x^k
             gradS   = this.getGradS(x_cur); % the gradient of the current iterate
-
+            
             err     = zeros(iter + 2,1);   % store the L2 norm with x_star
-            eng     = zeros(iter + 2,1);   % store the objective function values            
-            err(1)  = norm(x_cur - x_star,'fro'); 
-            eng(1)  = this.E(x_cur);  
+            eng     = zeros(iter + 2,1);   % store the objective function values
+            err(1)  = norm(x_cur - x_star,'fro');
+            eng(1)  = this.E(x_cur);
             x_array = cell(iter,1); % store all x values
             d_array = cell(iter,1); % intermediate variable for showing the rate of convergence
             
             convergeTol = 0;   % reach convergence tolerance or not
-
+            
             % initialization for the 1st step,
             switch method
-                case {'NIDS', 'NIDS-adaptive', 'NIDSS', 'NIDSS-adaptive'} 
-                    z       = x_cur - diag(alpha) * gradS;  % 
-                    x_new   = this.getProxR(z, alpha);      % 
+                case {'NIDS', 'NIDS-adaptive', 'NIDSS', 'NIDSS-adaptive'}
+                    z       = x_cur - diag(alpha) * gradS;  %
+                    x_new   = this.getProxR(z, alpha);      %
                     x_old   = x_cur;                        % increase k by 1
-                    x_cur   = x_new;                        % increase k by 1 
+                    x_cur   = x_new;                        % increase k by 1
                     gradS_old = gradS;                      % increase k by 1
                     gradS   = this.getGradS(x_cur);         % increase k by 1
                 case {'PG-EXTRA', 'PGEXTRA', 'EXTRA'}
-                    z       = W * x_cur - diag(alpha) * gradS; 
-                    x_new   = this.getProxR(z, alpha);     
+                    z       = W * x_cur - diag(alpha) * gradS;
+                    x_new   = this.getProxR(z, alpha);
                     x_old   = x_cur;                        % increase k by 1
-                    x_cur   = x_new;                        % increase k by 1 
+                    x_cur   = x_new;                        % increase k by 1
                     gradS_old = gradS;                      % increase k by 1
                     gradS   = this.getGradS(x_cur);         % increase k by 1
                 otherwise
-                    warning('Unexpected Method, Please choose from ?? ')                    
+                    warning('Unexpected Method, Please choose from ?? ')
             end
             err(2)  = norm(x_cur - x_star,'fro');
-            eng(2)  = this.E(x_cur);  
-         
+            eng(2)  = this.E(x_cur);
+            
             for i=1:iter
                 % x_new x^{k+1}, x_cur x^{k}, x_old x^{k-1}
                 %                gradS {k}, gradS_old {k-1}
                 switch method
-                    case {'NIDS', 'NIDS-adaptive', 'NIDSS', 'NIDSS-adaptive'}  
+                    case {'NIDS', 'NIDS-adaptive', 'NIDSS', 'NIDSS-adaptive'}
                         x_bar = 2 * x_cur - x_old - diag(alpha) * (gradS - gradS_old);
-                        z     = z - x_cur + tildeW * x_bar;
+                        z     = z - x_cur + tildeW(:,:,randi(len_W)) * x_bar;
                         x_new = this.getProxR(z, alpha);
-
+                        
                     case {'PG-EXTRA', 'PGEXTRA', 'EXTRA'}
-                        x_bar = 2 * x_cur - x_old; 
-                        z = z - x_cur + tildeW * x_bar - diag(alpha) * (gradS - gradS_old);
-                        x_new = this.getProxR(z, alpha);                        
+                        x_bar = 2 * x_cur - x_old;
+                        z = z - x_cur + tildeW(:,:,randi(len_W)) * x_bar - diag(alpha) * (gradS - gradS_old);
+                        x_new = this.getProxR(z, alpha);
                     otherwise
                         warning('Unexpected Method, Please choose from ?? ')
                 end
                 
-                x_array{i}  = x_new;                
+                x_array{i}  = x_new;
                 d_array{i}  = Q * (x_cur - x_new) - gradS;  % compute d based on (3.2) in the paper
-                x_old       = x_cur;                    % increase k by 1    
+                x_old       = x_cur;                    % increase k by 1
                 x_cur       = x_new;                    % increase k by 1
                 gradS_old   = gradS;                    % increase k by 1
                 gradS       = this.getGradS(x_cur);     % increase k by 1
                 err(i + 2)  = norm(x_cur-x_star,'fro');
-                eng(i + 2)  = this.E(x_cur); 
+                eng(i + 2)  = this.E(x_cur);
                 
                 if err(i + 2) < tol
                     x_array = x_array(1:i);
-                    d_array = d_array(1:i);                    
+                    d_array = d_array(1:i);
                     err 	= err(1:(i + 2));
                     eng 	= eng(1:(i + 2));
                     convergeTol = 1;
                     break
                 end
-            end;
+            end
             out.err 	= err;
             out.eng 	= eng;
             out.x_array = x_array;
             out.d_array = d_array;
             out.convergeTol = convergeTol;
         end
-
+        
         function [out] = minimize_DIGing(this, paras)
             tol     = paras.tol;    % tolerance
             x_star  = paras.x_star; % the optimal solution x
@@ -140,26 +147,27 @@ classdef NIDS
             gradS   = this.getGradS(x_cur); % the gradient of the current iterate
             y_cur   = gradS;
             alpha   = paras.alpha;  % the stepsize, which can be different for different nodes
-            iter    = paras.iter;   % the maximum number of iterations            
-            W       = paras.W;      % the mixing matrix            
+            iter    = paras.iter;   % the maximum number of iterations
+            W       = paras.W;      % the mixing matrix
+            [~,~,len_W] = size(W);
             atc     = paras.atc;    % whether to choose ATC or not
             err     = zeros(iter + 1,1);   % store the L2 norm with x_star
-            eng     = zeros(iter + 1,1);   % store the objective function values            
-            err(1)  = norm(x_cur - x_star,'fro'); 
-            eng(1)  = this.E(x_cur);  
+            eng     = zeros(iter + 1,1);   % store the objective function values
+            err(1)  = norm(x_cur - x_star,'fro');
+            eng(1)  = this.E(x_cur);
             x_array = cell(iter,1); % store all x values
             
             convergeTol = 0;                    % reach convergence tolerance or not
-
+            
             for i=1:iter
                 if atc == 1
-                    x_new = W * (x_cur - diag(alpha) * y_cur);
+                    x_new = W(:,:,randi(len_W)) * (x_cur - diag(alpha) * y_cur);
                     gradS_new = this.getGradS(x_new);
-                    y_new = W * (y_cur + gradS_new - gradS);
+                    y_new = W(:,:,randi(len_W)) * (y_cur + gradS_new - gradS);
                 else
-                    x_new = W * x_cur - diag(alpha) * y_cur;
+                    x_new = W(:,:,randi(len_W)) * x_cur - diag(alpha) * y_cur;
                     gradS_new = this.getGradS(x_new);
-                    y_new = W * y_cur + gradS_new - gradS;
+                    y_new = W(:,:,randi(len_W)) * y_cur + gradS_new - gradS;
                 end
                 x_array{i} = x_new;
                 
@@ -167,7 +175,7 @@ classdef NIDS
                 y_cur   = y_new;                          % increase k by 1
                 gradS   = gradS_new;                      % increase k by 1
                 err(i+1) = norm(x_cur-x_star,'fro');
-                eng(i+1) = this.E(x_cur);  
+                eng(i+1) = this.E(x_cur);
                 
                 if err(i+1)<tol
                     x_array = x_array(1:i);
@@ -176,141 +184,12 @@ classdef NIDS
                     convergeTol = 1;
                     break
                 end
-            end                        
+            end
             out.err = err;
             out.eng = eng;
             out.x_array = x_array;
             out.convergeTol = convergeTol;
         end
-        function [out] = minimize_cyclic(this, paras, w_array)
-            
-            % get all the parameters
-            tol     = paras.tol;    % tolerance
-            n       = paras.n;      % the number of nodes
-            p       = paras.p;      % the dimension of x on each nodes
-            x_star  = paras.x_star; % the optimal solution x
-            iter    = paras.iter;   % the maximum number of iterations
-            x0      = paras.x0;     % the initial x
-            alpha   = paras.alpha;  % the stepsize, which can be different for different nodes
-            method  = paras.method; % the method that used 
-            W       = w_array{1};      % the mixing matrix
-            
-            Q       = diag(1./alpha);
-            eye_L   = eye(n);
-            I_W     = eye_L - W;
-            
-            if isfield(paras, 'forcetTildeW') && paras.forcetTildeW
-                tildeW = (eye_L + W)/2;
-            else
-                switch method
-                    case {'NIDS', 'NIDS-adaptive', 'NIDSS', 'NIDSS-adaptive'}  % NIDS
-                        c = paras.c;      % the parameter c in  NIDS
-
-                        tildeW = eye_L - c * diag(alpha) * I_W;
-                    case {'PG-EXTRA', 'PGEXTRA', 'EXTRA'}
-                        tildeW = (eye_L + W)/2;
-                end
-            end
-            
-            x_cur   = x0;           % the current iterate   x^k 
-            gradS   = this.getGradS(x_cur); % the gradient of the current iterate
-
-            err     = zeros(iter + 2,1);   % store the L2 norm with x_star
-            eng     = zeros(iter + 2,1);   % store the objective function values            
-            err(1)  = norm(x_cur - x_star,'fro'); 
-            eng(1)  = this.E(x_cur);  
-            x_array = cell(iter,1); % store all x values
-            d_array = cell(iter,1); % intermediate variable for showing the rate of convergence
-            
-            convergeTol = 0;   % reach convergence tolerance or not
-
-            % initialization for the 1st step,
-            switch method
-                case {'NIDS', 'NIDS-adaptive', 'NIDSS', 'NIDSS-adaptive'} 
-                    z       = x_cur - diag(alpha) * gradS;  % 
-                    x_new   = this.getProxR(z, alpha);      % 
-                    x_old   = x_cur;                        % increase k by 1
-                    x_cur   = x_new;                        % increase k by 1 
-                    gradS_old = gradS;                      % increase k by 1
-                    gradS   = this.getGradS(x_cur);         % increase k by 1
-                case {'PG-EXTRA', 'PGEXTRA', 'EXTRA'}
-                    z       = W * x_cur - diag(alpha) * gradS; 
-                    x_new   = this.getProxR(z, alpha);     
-                    x_old   = x_cur;                        % increase k by 1
-                    x_cur   = x_new;                        % increase k by 1 
-                    gradS_old = gradS;                      % increase k by 1
-                    gradS   = this.getGradS(x_cur);         % increase k by 1
-                otherwise
-                    warning('Unexpected Method, Please choose from ?? ')                    
-            end
-            err(2)  = norm(x_cur - x_star,'fro');
-            eng(2)  = this.E(x_cur);  
-         
-            for i=1:iter
-               
-                %new code, recalc W
-                w_val = mod(i-1, length(w_array))+1;
-                W       = w_array{w_val};      % the mixing matrix
-                eye_L   = eye(n);
-                I_W     = eye_L - W;
-
-                if isfield(paras, 'forcetTildeW') && paras.forcetTildeW
-                    tildeW = (eye_L + W)/2;
-                else
-                    switch method
-                        case {'NIDS', 'NIDS-adaptive', 'NIDSS', 'NIDSS-adaptive'}  % NIDS
-                            tildeW = eye_L - c * diag(alpha) * I_W;
-                        case {'PG-EXTRA', 'PGEXTRA', 'EXTRA'}
-                            tildeW = (eye_L + W)/2;
-                    end
-                end
-                %end new code
-                
-                
-                disp(tildeW)
-                
-                % x_new x^{k+1}, x_cur x^{k}, x_old x^{k-1}
-                %                gradS {k}, gradS_old {k-1}
-                switch method
-                    case {'NIDS', 'NIDS-adaptive', 'NIDSS', 'NIDSS-adaptive'}  
-                        x_bar = 2 * x_cur - x_old - diag(alpha) * (gradS - gradS_old);
-                        z     = z - x_cur + tildeW * x_bar;
-                        x_new = this.getProxR(z, alpha);
-
-                    case {'PG-EXTRA', 'PGEXTRA', 'EXTRA'}
-                        x_bar = 2 * x_cur - x_old; 
-                        z = z - x_cur + tildeW * x_bar - diag(alpha) * (gradS - gradS_old);
-                        x_new = this.getProxR(z, alpha);                        
-                    otherwise
-                        warning('Unexpected Method, Please choose from ?? ')
-                end
-                
-                x_array{i}  = x_new;                
-                d_array{i}  = Q * (x_cur - x_new) - gradS;  % compute d based on (3.2) in the paper
-                x_old       = x_cur;                    % increase k by 1    
-                x_cur       = x_new;                    % increase k by 1
-                gradS_old   = gradS;                    % increase k by 1
-                gradS       = this.getGradS(x_cur);     % increase k by 1
-                err(i + 2)  = norm(x_cur-x_star,'fro');
-                eng(i + 2)  = this.E(x_cur); 
-                
-                if err(i + 2) < tol
-                    x_array = x_array(1:i);
-                    d_array = d_array(1:i);                    
-                    err 	= err(1:(i + 2));
-                    eng 	= eng(1:(i + 2));
-                    convergeTol = 1;
-                    break
-                end
-            end;
-            out.err 	= err;
-            out.eng 	= eng;
-            out.x_array = x_array;
-            out.d_array = d_array;
-            out.convergeTol = convergeTol;
-        end
-
-       
-       
+        
     end
 end
